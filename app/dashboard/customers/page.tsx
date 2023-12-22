68a8112dbef34da30590e9ba04536c97b22766b2
Khun Kyaw Kham Hti <khamhti@agga.io>
2023-12-22 22:59:55 +0630

    add 3 more pages
    dashboard, font,

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,3 @@
+export default function Page () {
+    return <p>customer is no more always right !!! customer page </p>
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,3 @@
+export default function Page () {
+    return <p>haha i got it back!!!! this is invoices</p>;
+}
