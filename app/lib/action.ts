773ad18c2d749f4c603f929f3bd73a60d7b4f0b1
Khun Kyaw Kham Hti <khamhti@agga.io>
2024-03-08 13:25:30 +0630

    adding authentication

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -3,6 +3,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn, signOut } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -30,7 +32,6 @@ export type State = {
   };
   message?: string | null;
 };
-
 
 export async function createInvoice(prevState: State, formData: FormData) {
 
@@ -115,7 +116,7 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
 };
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
+  // throw new Error('Failed to Delete Invoice');
 
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
@@ -126,4 +127,23 @@ export async function deleteInvoice(id: string) {
   }
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
